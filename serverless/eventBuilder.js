f706a1b46d8bda16005d3ce0bfd011a1090356c1
Amar kumar singh <amar@ostrojs.com>
2025-07-23 12:06:06 +0530

    Develop (#19)
    * Update package.json
    
    * Update package.json
    
    * serverless feature
    
    * Update package.json
    
    * package version change
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * default server type is server
    
    * Update package.json
    
    * offline request object for serverless
    
    * Update package.json
    
    * subprocess for serverless offlice
    
    * handler base64 response
    
    * console log print for serverless offline
    
    * added cookie and other keys on event obj

--- serverless/eventBuilder.js
+++ serverless/eventBuilder.js
@@ -1,4 +1,5 @@
 const url = require('url');
+const { Buffer } = require('buffer');
 
 class EventBuilder {
     static fromHttpRequest(req) {
@@ -6,29 +7,52 @@ class EventBuilder {
             const bodyChunks = [];
             req.on('data', chunk => bodyChunks.push(chunk));
             req.on('end', () => {
-                const body = Buffer.concat(bodyChunks).toString();
+                const rawBody = Buffer.concat(bodyChunks);
+                const isBinary = EventBuilder._isBinaryContent(req.headers['content-type']);
+                const body = isBinary ? rawBody.toString('base64') : rawBody.toString();
+
                 const parsedUrl = url.parse(req.url, true);
+                const headers = req.headers;
+
+                const cookies = headers.cookie
+                    ? headers.cookie.split(';').map(c => c.trim())
+                    : [];
+
                 resolve({
                     version: '2.0',
                     routeKey: parsedUrl.pathname,
                     rawPath: parsedUrl.pathname,
-                    rawQueryString: parsedUrl.search ? parsedUrl.search.slice(1) : '',
-                    headers: req.headers,
+                    rawQueryString: parsedUrl.query
+                        ? new URLSearchParams(parsedUrl.query).toString()
+                        : '',
+                    cookies,
+                    headers,
                     requestContext: {
                         http: {
                             method: req.method,
                             path: parsedUrl.pathname,
                             protocol: `HTTP/${req.httpVersion}`,
-                            sourceIp: req.socket.remoteAddress,
-                            userAgent: req.headers['user-agent'] || ''
+                            sourceIp: req.socket.remoteAddress || '',
+                            userAgent: headers['user-agent'] || '',
                         },
-                        timeEpoch: Date.now()
+                        timeEpoch: Date.now(),
                     },
-                    isBase64Encoded: false,
-                    body
+                    isBase64Encoded: isBinary,
+                    body,
                 });
             });
         });
+    }
+
+    static _isBinaryContent(contentType = '') {
+        const textTypes = [
+            'text/',
+            'application/json',
+            'application/xml',
+            'application/javascript',
+            'application/x-www-form-urlencoded',
+        ];
+        return !textTypes.some(t => contentType.startsWith(t));
     }
 }
 
